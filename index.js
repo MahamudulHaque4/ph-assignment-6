ac9411c4d6734f512793eb3e4aad2f964b10bdb3
MahamudulHaque4 <hratul183@gmai.com>
2025-09-13 10:51:20 +0600

    Api and JSON part updated and modified

--- index.js
+++ index.js
@@ -109,5 +109,114 @@ const addActiveClass = (className) => {
   });
 };
 
+let cart = [];
+let cartContainer = document.querySelector(".cart-container");
+let totalCart = document.querySelector(".total-cart");
+
+const addToCart = (treeName, price, id) => {
+  let count = 1;
+  let matchedItem = cart.find((cartItem) => cartItem.id === id);
+
+  if (matchedItem) {
+    matchedItem.price += price;
+    matchedItem.count += 1;
+    displayAddToCart(id);
+    sumOfTotalPrice();
+    return;
+  }
+
+  cart.unshift({ treeName, price, id, count });
+  displayAddToCart(id);
+  sumOfTotalPrice();
+  totalCart.innerText = cart.length;
+};
+
+const removeCart = (id) => {
+  cart = cart.filter((cartItem) => cartItem.id !== id);
+
+  displayAddToCart();
+  sumOfTotalPrice();
+  totalCart.innerText = cart.length;
+};
+
+const displayAddToCart = () => {
+  cartContainer.innerHTML = cart
+    .map((cartItem) => {
+      return `
+        <div class="flex items-center justify-between p-2 rounded-sm bg-[#F0FDF4] cursor-pointer">
+            <div>
+              <h2 class="font-bold mb-1">${cartItem.treeName}</h2>
+              <span class="text-[#1F2937]/50">৳ ${
+                cartItem.price / cartItem.count
+              } x ${cartItem.count}</span> = ${cartItem.price}
+            </div>
+            <span onclick="removeCart('${
+              cartItem.id
+            }')" class="cursor-pointer bg-gray-100 p-2 rounded-md text-red-600/70"><i class="fa-solid fa-trash text-sm"></i></span>
+        </div>
+    `;
+    })
+    .join(" ");
+};
+
+// *****
+// Total Price of Add To Card Added
+// *****
+let totalPrice = document.querySelector(".total-price");
+
+const sumOfTotalPrice = () => {
+  let total = cart.reduce((sum, cartItem) => {
+    sum += cartItem.price;
+    return sum;
+  }, 0);
+  totalPrice.innerText = total;
+};
+
+// *****
+// Loading Spinner Functionality
+// *****
+const loadingSpinner = (status, place) => {
+  if (status) {
+    place.innerHTML = `<div class="col-span-full text-center mt-20"><span class="loading loading-dots loading-xl"></span></div>`;
+  }
+};
+
+// Showing Dialog Box for every single card detail
+let dialogBox = document.getElementById("card_details");
+const loadDetails = async (id) => {
+  dialogBox.showModal();
+  let detailsUrl = `https://openapi.programming-hero.com/api/plant/${id}`;
+
+  let response = await fetch(detailsUrl);
+  let data = await response.json();
+
+  displayDetails(data.plants);
+};
+
+const displayDetails = (detail) => {
+  dialogBox.innerHTML = `
+   <div class="modal-box p-3">
+      <div> 
+        <h1 class="text-2xl font-bold mb-2">${detail.name} <h1>
+        <div class="w-full max-h-[320px] overflow-hidden">
+          <img class="w-full h-full aspect-video object-cover  rounded-md" src="${detail.image}"> 
+        </div>
+        <p class="mt-2"> <span class="font-bold">Category:</span> ${detail.category}</p>
+        <p class="mt-2"> <span class="font-bold">Price:</span> $ ${detail.price}</p>
+        <p class="mt-2"> <span class="font-bold">Description:</span> ${detail.description}</p>
+
+
+      </div>
+
+      <div class="modal-action">
+        <form method="dialog">
+          <button class="btn">Close</button>
+        </form>
+      </div>
+  </div>
+  `;
+};
+
+
 loadCategory();
 loadCategoryItem("plants");
